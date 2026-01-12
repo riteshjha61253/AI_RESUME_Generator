bab4d3e83e6029c6d193c28027e45a12a0d9eb6d
riteshjha61253 <jha61253@gmail.com>
2026-01-13 01:07:08 +0530

    Changed path

--- resume_frontend/src/main.jsx
+++ resume_frontend/src/main.jsx
@@ -26,7 +26,7 @@ createRoot(document.getElementById("root")).render(
           <Route path="contact" element={<Contact />} />
           {/* <Route path="generate-resume" element={<GenerateResume />} /> */}
  <Route path="generate-resume" element={<GenerateResume />} />
- <Route path="/register" element={<Register />} />
+ <Route path="register" element={<Register />} />
 
 
           <Route path="login" element={<Login />} />
